12cc1f7778f3fa2dc77eeedc67ab4f22aef0f261
HarryCelt <harrycelt2@gmail.com>
2022-04-11 17:35:56 -0700

    Back again

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - prints the phrase and that piece of art is useful
+ * \" - Dora Korpar, 2O15-1O-l9
+ * Return: l if the program works correctly, O(zero) otherwise
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
+	return (1);
+}
