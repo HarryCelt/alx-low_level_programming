925146b2405b7e121a50203565f66710cd2f5133
HarryCelt <harrycelt2@gmail.com>
2022-04-11 17:49:00 -0700

    Back again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase \"Programming is like building a multilingual puzzle
- * Return: should end with a return value of zero, if it is correct and non zero if otherwise.
+ * main - print the phrase \"Programming is like building a multilingual puzzle
+ * Return: should end with a return value O(zero) and non-zero otherwise.
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
