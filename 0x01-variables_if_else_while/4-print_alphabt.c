61b4ae756de617738b704646da017905c6eb51f5
HarryCelt <harrycelt2@gmail.com>
2022-04-22 00:36:18 -0700

    Update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - create char alph
+ * loops - for, if, else if
+ * Return: 0
+ */
+int main(void)
+{
+	char alph;
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph == 'q')
+			continue;
+		else if (alph == 'e')
+			continue;
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
