c605a70151d47da06328d0afab739e3ee21b354a
HarryCelt <harrycelt2@gmail.com>
2022-04-21 23:55:54 -0700

    Update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints the phrase and that piece of art is useful
- * \" - Dora Korpa, 2O15-1O-l9
+ * \" - Dora Korpar, 2O15-1O-l9
  * Return: l if the program works correctly, O(zero) otherwise
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
