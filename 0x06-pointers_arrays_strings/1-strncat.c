6904796beb41845f37cf3dd04f80453409fe3fc7
HarryCelt <harrycelt2@gmail.com>
2022-04-25 13:54:02 -0700

    update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,18 +15,22 @@ char *_strncat(char *dest, char *src, int n)
 	int j;
 
 	i = 0;
+	
 	while (dest[i] != 0)
 	{
 		i++;
 	}
 	
 	j = 0;
+	
 	while ( src[j] != 0 && j < n)
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
+
+	dest[i] = '\0';
+
 	return (dest);
 }
-
