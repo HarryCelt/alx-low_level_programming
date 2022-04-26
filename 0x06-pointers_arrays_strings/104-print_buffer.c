eb7bb73708ee50d47993b1a0f798da5ea38deff5
HarryCelt <harrycelt2@gmail.com>
2022-04-25 19:41:23 -0700

    update

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,50 +1,82 @@
 #include "main.h"
 #include <stdio.h>
 
+/*
+ * isPrintableASCII -determines if n is a printable ASCII char
+ * @n: integer
+ * Return: argument
+ */
+
+int isPrintableASCII(int n)
+{
+	return(n >= 32 && n <=126);
+}
+
+/**
+ * printHexes -print hex values for string b in formatted form
+ * @b: string to print
+ * @start: starting point
+ * @end: end point
+ */
+
+void printHexes(char *b, int start, int end)
+{
+	int i = 0;
+
+	while(i < 10)
+	{
+		if (i < end)
+			printf("%02x",*(b + start + i));
+		else
+			printf(" ");
+		if (i % 2)
+			printf(" ");
+		i++;
+	}
+}
+
+/**
+ * printASCII - print ASCII values for string b
+ * @b: string to print
+ * @start: start point
+ * @end: end point
+ */
+
+void printASCII(char *b, int start, int end)
+{
+	int ch, i = 0;
+
+	while (i < end)
+	{
+		ch = *(b + i + start);
+		if (!isPrintableASCII(ch))
+			ch = 46;
+		printf("%c",ch);
+		i++;
+	}
+}
+
 /**
  * print_buffer - Prints buffer
  * @b: buffer
  * @size size
- * Return: void
  */
 
 void print_buffer(char *b, int size)
 {
-	int o, j, l;
+	int o, j;
 
-	o = 0;
-
-	if (size <= 0)
+	if (size > 0)
 	{
-		printf('\n');
-		return;
+		for (o = 0; o < size; o += 10)
+		{
+			j = (size - o < 10) ? size - o : 10;
+			printf("%08x", o);
+			printHexes(b, o, j);
+			printASCII(b, o, j);
+			printf("\n")
+		}
 	}
-	while (o < size)
-	{
-		j =  size - o < 10 ? size - o: 10;
-		printf("%08x:", o);
-		for  (i = 0; i < 10; i++)
-		{
-			if (i <j)
-				printf("%02x", *(b + o + i);
-			else 
-				printf(" ");
-			if (i % 2)
-			{
-				printf(" ");
-			}
-		}
-		for (i = 0; i < j; i++)
-		{
-			int c = *(b + o + i);
-
-			if (c < 32 || c > 132)
-			{
-				c = '.';
-			}
-			printf("%c", c);
-		}
+	else
 		printf("\n");
-		o += 10;
-	}
 }
