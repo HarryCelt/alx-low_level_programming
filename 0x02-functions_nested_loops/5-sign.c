520f883c3a44460b0ae453ba83a59aa5a254db77
HarryCelt <harrycelt2@gmail.com>
2022-05-10 06:10:11 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - when letter is 
+ * lowercase = 0, uppercase = 1
+ * @n: the int to print
+ * Return: 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+
+}
