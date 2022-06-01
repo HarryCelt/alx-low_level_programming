c80b524262184d7ecd79adaa83ea36c1d82089c7
HarryCelt <harrycelt2@gmail.com>
2022-05-31 19:05:23 -0700

    bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,12 +9,11 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	bit_value;
-
+	int bit_value;
+	
 	if (index > 63)
 		return (-1);
-
 	bit_value = (n >> index) & 1;
-
+	
 	return (bit_value);
 }
